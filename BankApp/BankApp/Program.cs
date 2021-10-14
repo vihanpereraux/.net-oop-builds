eaba6004979a557d484817e88f779ff5b24e52d9
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2021-10-14 23:34:32 +0530

    Exception test methods added

--- BankApp/BankApp/Program.cs
+++ BankApp/BankApp/Program.cs
@@ -6,13 +6,21 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            //Practice question
             Program p1 = new Program();
+            // Practice question
             p1.abstIntfPractice();
+
+            // Question 05
             p1.questionFive();
+
+            // Exceptions - Practice
+            p1.exceptionPractice();
+
+            // Exceptions - Practice 02
+            p1.exceptionPracticeTwo();
         }
 
-        //Practice question - Method
+        // Practice question - Method
         public void abstIntfPractice()
         {
             Circle circle1 = new Circle();
@@ -25,6 +33,7 @@ namespace BankApp
             Console.ReadLine();
         }
 
+        // Question 05 - Method
         public void questionFive()
         {
             IAccount savingAccount = new SavingAccount();
@@ -33,5 +42,56 @@ namespace BankApp
             currentAccount.printAccountData();
             Console.ReadLine();
         }
+
+        // Exceptions - Practice - Method
+        public void exceptionPractice()
+        {
+            try
+            {
+                Console.Write("Enter number : ");
+                int number = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Your number is : {number}.");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Exception occured - Error - {e.Message}");
+            }
+            
+            Console.ReadLine();
+        }
+
+        // Exceptions - Practice 02 - Method
+        public void exceptionPracticeTwo()
+        {
+            try
+            {
+                int n1 = 0;
+                int n2 = 0;
+                int div = 0;
+
+                Console.Write("Enter number one : ");
+                n1 = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Ënter number two : ");
+                n2 = Int32.Parse(Console.ReadLine());
+
+                div = n1 / n2;
+            }
+            catch (FormatException e) 
+            {
+                Console.WriteLine($"Exception occured - Error - {e.Message}");
+            }
+            catch (DivideByZeroException e) 
+            {
+                Console.WriteLine($"Exception occured - Error - {e.Message}");
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine($"Exception occured - Error - {e.Message}");
+            }
+        }
+
+
     }
 }
